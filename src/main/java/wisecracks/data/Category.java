1bdfca737359ba9e0b457064425bc2722e2b2e94
Utkarsha Shetye <ums.shetye@gmail.com>
2015-12-04 12:45:20 -0500

    added db table changes

--- src/main/java/wisecracks/data/Category.java
+++ src/main/java/wisecracks/data/Category.java
@@ -1,19 +1,56 @@
 class Category{
 
-    public Category(){
-        int categoryId;
-        int categoryName;
+    int categoryId;
+    String categoryName;
+
+    //Default contructor
+    public Category(String categoryName){
+        //retrieve CategoryId from DB for the given CategoryName
+    }
+
+    //Secondary Constructor
+    public Category(int categoryId, String categoryName){
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
     }
 }
 
 class User{
-    User(){
+
+    int userId; // Change to int in Db
+    String firstName;
+    String lastName;
+    String addressLine1;
+    String addressLine2;
+    String city;
+    String stateId;
+    String profile;
+    Date recentVisitedTime;
+
+    //Default constructor
+    public User(){
+    }
+
+    //Secondary constructor
+    public User(){
 
     }
 }
 
 class Subscriber{
-    Subscriber(){
+
+    int subscriberId; //change to int in Db //Do we even need this ?
+    int userId;
+    int forumId;
+    Date subscriptionTime;
+
+    //Default contructor
+    public Subscriber(){
+
+    }
+
+    //Secondary constructor
+    public Subscriber(){
 
     }
 }
