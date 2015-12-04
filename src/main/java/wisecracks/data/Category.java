958bd5e9528e6f3205b55722351679a99824fdc9
Utkarsha Shetye <ums.shetye@gmail.com>
2015-12-04 09:32:13 -0500

    data folder

---
+++ src/main/java/wisecracks/data/Category.java
@@ -0,0 +1,19 @@
+class Category{
+
+    public Category(){
+        int categoryId;
+        int categoryName;
+    }
+}
+
+class User{
+    User(){
+
+    }
+}
+
+class Subscriber{
+    Subscriber(){
+
+    }
+}
