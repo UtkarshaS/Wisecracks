631bbe30463034ae666073c3d5409ed6c0f8d9f2
Utkarsha Shetye <ums.shetye@gmail.com>
2015-12-04 14:42:40 -0500

    Added support to handle call to login controller

--- src/main/java/wisecracks/controller/LoginController.java
+++ src/main/java/wisecracks/controller/LoginController.java
@@ -2,9 +2,24 @@ package main.java.wisecracks.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 
 class LoginController {
-    public void login(HttpServletRequest request, HttpServletResponse response, String message, String parameters) {
+    public void login(HttpServletRequest request, HttpServletResponse response, int userId) {
 
+        boolean userExists = //RDS Search query;
+        if(!userExists)
+        {
+            //Create user entry in DB
+            String firstName =; // retrive from Google
+            String lastName =; // retrive from Google
+
+            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            //get current date time with Date()
+            Date date = new Date();
+            Date current = dateFormat.format(date);
+
+            User(userId, firstName, lastName, );
+        }
     }
 }
