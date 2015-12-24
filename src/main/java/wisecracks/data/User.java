85b5b7fb8c00727d3a2d6cf2316b19f346516835
shweta-sachdeva <ss8740@nyu.edu>
2015-12-23 19:29:38 -0500

    updating user.java

--- src/main/java/wisecracks/data/User.java
+++ src/main/java/wisecracks/data/User.java
@@ -2,18 +2,17 @@ package main.java.wisecracks.data;
 
 public class User {
 
-    final String userId;
-    final String firstName;
-    final String lastName;
-    final String addressLine1;
-    final String addressLine2;
-    final String city;
-    final String country;
-    final String profile;
-    final String registerationEndPoint;
-
+     String userId;
+     String firstName;
+     String lastName;
+     String addressLine1;
+     String addressLine2;
+     String city;
+     String country;
+     String profile;
+    String registrationEndPoint;
     public User(String userId, String firstName, String lastName, String addressLine1, String addressLine2,
-	    String city, String country, String profile,String registerationEndPoint ) {
+	    String city, String country, String profile, String registrationEndPoint) {
 	this.userId = userId;
 	this.firstName = firstName;
 	this.lastName = lastName;
@@ -22,7 +21,7 @@ public class User {
 	this.city = city;
 	this.country = country;
 	this.profile = profile;
-    this.registerationEndPoint = registerationEndPoint;
+	this.registrationEndPoint= registrationEndPoint;
     }
 
 }
